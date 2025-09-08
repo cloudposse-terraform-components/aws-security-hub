46abd6bc82a24e8fcfd0e7ae5061fe19495dc07e
Sharon Dagan <sharon.dagan@quanthealth.ai>
2025-09-08 20:07:33 +0300

    Allow users to set component name in remote state (#39)
    * Update variables.tf
    
    * Update remote-state.tf

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -2,7 +2,7 @@ module "account_map" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
   version = "1.8.0"
 
-  component   = "account-map"
+  component   = var.account_map_component_name
   tenant      = var.account_map_tenant != "" ? var.account_map_tenant : module.this.tenant
   stage       = var.root_account_stage
   environment = var.global_environment
